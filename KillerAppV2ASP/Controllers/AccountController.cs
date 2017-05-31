173768094d6a62aeb773560f363658ca8eee5e85
Remi van Deursen <cryslo1994@gmail.com>
2017-05-31 23:02:22 +0200

    Text input progressie

--- KillerAppV2ASP/Controllers/AccountController.cs
+++ KillerAppV2ASP/Controllers/AccountController.cs
@@ -2,16 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using KillerAppV2ASP.Models;
 
 namespace KillerAppV2ASP.Controllers
 {
     public class AccountController : Controller
     {
+        static List<User> users = new List<User>();
         // GET: Account
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var user = new User()
+            {
+                Password = "pass",
+                Username = "user"
+            };
+            return View(user);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Login(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", user);
+            }
+            users.Add(user);
+            return RedirectToAction("Index");
         }
     }
 }
--- KillerAppV2ASP/Models/DBO/RPGSQLContext.cs
+++ KillerAppV2ASP/Models/DBO/RPGSQLContext.cs
@@ -63,33 +63,6 @@ namespace KillerAppV2ASP.Models.DBO
 
         public bool TryLogin(string username, string password)
         {
-            int userId = 0;
-            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Validate_User"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Username", Login1.UserName);
-                    cmd.Parameters.AddWithValue("@Password", Login1.Password);
-                    cmd.Connection = con;
-                    con.Open();
-                    userId = Convert.ToInt32(cmd.ExecuteScalar());
-                    con.Close();
-                }
-                switch (userId)
-                {
-                    case -1:
-                        Login1.FailureText = "Username and/or password is incorrect.";
-                        break;
-                    case -2:
-                        Login1.FailureText = "Account has not been activated.";
-                        break;
-                    default:
-                        FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
-                        break;
-                }
-            }
             throw new NotImplementedException();
         }
     }
