b3f0350faa5cc3bea40aadaaec8301fde609d9ba
Remi van Deursen <cryslo1994@gmail.com>
2017-05-31 19:05:14 +0200

    Testing some features. Now working no authentication.

--- KillerAppV2ASP/Models/DBO/RPGSQLContext.cs
+++ KillerAppV2ASP/Models/DBO/RPGSQLContext.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 
 namespace KillerAppV2ASP.Models.DBO
 {
@@ -61,6 +63,33 @@ namespace KillerAppV2ASP.Models.DBO
 
         public bool TryLogin(string username, string password)
         {
+            int userId = 0;
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("Validate_User"))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Username", Login1.UserName);
+                    cmd.Parameters.AddWithValue("@Password", Login1.Password);
+                    cmd.Connection = con;
+                    con.Open();
+                    userId = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                }
+                switch (userId)
+                {
+                    case -1:
+                        Login1.FailureText = "Username and/or password is incorrect.";
+                        break;
+                    case -2:
+                        Login1.FailureText = "Account has not been activated.";
+                        break;
+                    default:
+                        FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
+                        break;
+                }
+            }
             throw new NotImplementedException();
         }
     }
